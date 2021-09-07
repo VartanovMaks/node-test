d223f70e78bb603750cb633361c832b32a63791b
VartanovMaks <vartanov2009@gmail.com>
2021-09-07 21:37:53 +0300

    must work with atlas

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
+/* eslint-disable linebreak-style */
 const express = require('express');
-const fs=require('fs');
 const path = require('path');
 const cors = require('cors');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const router = require('./routes/film.router');
-const uri = "insert here ref to database";
+
+const uri = 'insert here ref to database';
 
 const app = express();
+// eslint-disable-next-line no-use-before-define
 _mongooseConnector();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
-const staticPath =path.join(__dirname, 'data');
+const staticPath = path.join(__dirname, 'data');
 app.use(express.static(staticPath));
 
-app.use('/films', router)
-
+app.use('/films', router);
 
 app.listen(3001, () => {
-    console.log('App listen 3001');
-  })
+  // eslint-disable-next-line no-console
+  console.log('App listen 3001');
+});
 
-  function _mongooseConnector() {
-    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, })
-      .then((con) => {
-        console.log(con.connection);
-        console.log('MongoDb connected')
-      })
-      .catch(err => console.log(err));
+// eslint-disable-next-line no-underscore-dangle
+function _mongooseConnector() {
+  mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+    .then(() => {
+      // eslint-disable-next-line no-console
+      console.log('MongoDb connected');
+    })
+    // eslint-disable-next-line no-console
+    .catch((err) => console.log(err));
   // mongo.connect('mongodb://localhost:27017/horror-films', { useNewUrlParser: true, useUnifiedTopology: true });
 }
--- controllers/film.controller.js
+++ controllers/film.controller.js
@@ -1,27 +1,27 @@
-const {Film} = require('../dataBase/index');
+/* eslint-disable linebreak-style */
+const { Film } = require('../dataBase/index');
 
 module.exports = {
 
-    getAllFilms : async (req,res, next)=>{
-        try {
-            const films = await Film.find({});
-            console.log(films);
-            res.json(films);
-        } catch (e){
-            next(e);
-        }
-    },
-
-    getFilmById : async (req,res, next)=>{
-        id= req.params.filmID;
-        try {
-            const film = await Film.findById(id);
-            if (!film) {
-                throw new Error(`film with id:${id} not found`);
-              }
-            res.json(film);
-        } catch(e){
-            next(e);
-        }
+  getAllFilms: async (req, res, next) => {
+    try {
+      const films = await Film.find({});
+      res.json(films);
+    } catch (e) {
+      next(e);
     }
-}
+  },
+
+  getFilmById: async (req, res, next) => {
+    const id = req.params.filmID;
+    try {
+      const film = await Film.findById(id);
+      if (!film) {
+        throw new Error(`film with id:${id} not found`);
+      }
+      res.json(film);
+    } catch (e) {
+      next(e);
+    }
+  },
+};
