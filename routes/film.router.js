64eeb4b2457fdea5841556c3a9a640ff0ccdb757
AlexP999 <mikol999@gmail.com>
2021-09-10 22:23:55 +0300

    й

--- routes/film.router.js
+++ routes/film.router.js
@@ -1,6 +1,11 @@
+/* eslint-disable linebreak-style */
 const router = require('express').Router();
+
 const filmController = require('../controllers/film.controller');
+const { filmMiddleware } = require('../middlewares');
 
 router.get('/', filmController.getAllFilms);
 router.get('/:filmID', filmController.getFilmById);
+router.post('/', filmMiddleware.checkFilmValidity, filmController.createFilm);
+
 module.exports = router;
